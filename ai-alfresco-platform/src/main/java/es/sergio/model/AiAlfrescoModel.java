2e7e0a42f2407c170dfef34b0666de371b9e6cd6
Sergio Cea <sergioceagomez@gmail.com>
2025-08-22 03:51:23 +0200

    [FIX] Fixed issue with adding invoice appearance when reading ticket

--- ai-alfresco-platform/src/main/java/es/sergio/model/AiAlfrescoModel.java
+++ ai-alfresco-platform/src/main/java/es/sergio/model/AiAlfrescoModel.java
@@ -5,13 +5,13 @@ import org.alfresco.service.namespace.QName;
 public class AiAlfrescoModel {
     public static final String AI_ALFRESCO_MODEL_1_0_URI = "http://www.ai-alfresco.org/model/content/1.0";
 
-    //Ticket Metadata Model
+    // Ticket Metadata Model
     public static final QName METADATA_TICKET_COUNTRY = QName.createQName(AI_ALFRESCO_MODEL_1_0_URI, "ticketCountry");
-    public static final QName METADATA_TICKET_DATE = QName.createQName(AI_ALFRESCO_MODEL_1_0_URI, "invoiceDate");
+    public static final QName METADATA_TICKET_DATE = QName.createQName(AI_ALFRESCO_MODEL_1_0_URI, "ticketDate");
     public static final QName METADATA_TICKET_TAX = QName.createQName(AI_ALFRESCO_MODEL_1_0_URI, "ticketTax");
     public static final QName METADATA_TICKET_TOTAL = QName.createQName(AI_ALFRESCO_MODEL_1_0_URI, "ticketTotal");
 
-    //Invoice Metadata Model
+    // Invoice Metadata Model
     public static final QName METADATA_INVOICE_DATE = QName.createQName(AI_ALFRESCO_MODEL_1_0_URI, "invoiceDate");
     public static final QName METADATA_INVOICE_TAX = QName.createQName(AI_ALFRESCO_MODEL_1_0_URI, "invoiceTax");
     public static final QName METADATA_INVOICE_TOTAL = QName.createQName(AI_ALFRESCO_MODEL_1_0_URI, "invoiceTotal");
